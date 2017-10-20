1efb56257f62130304ccd7e61ee38cf13c2dc005
Anderson Taborga <anderson.taborga@gmail.com>
2017-10-20 13:09:11 -0200

    Criação de classes no ES6, definição de construtores, propriedades, parametros, convenção de atributos com prefixo "_", métodos get como propriedade, retorno do atributo privado, object freeze e programação defensiva usando novos objetos.

---
+++ js/app/models/Negociacao.js
@@ -0,0 +1,29 @@
+class Negociacao {
+
+    //Propriedades com modificadores de acesso usando a conveção "_"
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    //Usando get, transformo o método em uma chamada por propriedade
+    get volume() {
+        return this._quantidade * this.valor;
+    }
+
+    get data() {
+        return new Date(this._data.getTime());
+    }
+
+    get quantidade() {
+        return this._quantidade;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+
+}
