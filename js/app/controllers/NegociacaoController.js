9a42edb6b60137ac0517a2c4cf3de0258d5d55fd
Anderson Taborga <anderson.taborga@gmail.com>
2017-11-07 22:02:34 -0200

    Usado arrow function em _listaNegociacoes para usar o contexto da NegociacaoController

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,7 +6,7 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
@@ -20,13 +20,20 @@ class NegociacaoController {
 
         event.preventDefault();
         this._listaNegociacoes.adciciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = 'Negociação adicionada com sucesso';
         this._mensagemView.update(this._mensagem);
 
         this._limpaFormulario();
        
+    }
+
+    apaga() {
+
+        this._listaNegociacoes.esvazia();
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
     }
 
     _criaNegociacao() {
--- js/app/models/ListaNegociacoes.js
+++ js/app/models/ListaNegociacoes.js
@@ -1,16 +1,23 @@
 class ListaNegociacoes {
 
-    constructor(){
+    constructor(armadilha){
 
         this._negociacoes = [];
+        this._armadilha = armadilha;
     }
 
     adciciona(negociacao){
         this._negociacoes.push(negociacao);
+        this._armadilha(this);
     }
 
     //Retorna a lista de negociações
     get negociacoes(){
         return [].concat(this._negociacoes);    
+    }
+
+    esvazia() {
+        this._negociacoes = [];
+        this._armadilha(this);
     }
 }
